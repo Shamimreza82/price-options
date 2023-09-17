9e54a4c828f765a122968da1b67b0d2f122b5015
Shamimreza82 <shamimrezaone@gmail.com>
2023-09-17 19:18:45 +0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DaisyNav from "./components/daisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className="text-2xl">Vite + React</h1>
+
+    </>
+  );
+}
+
+export default App;
